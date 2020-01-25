b874c911fa8d6896ab4f9a4c8c5424e79a67f281
theMladyPan <stanislav@rubint.sk>
2020-01-26 00:21:23 +0100

    working. EZ

--- src/ga.py
+++ src/ga.py
@@ -14,6 +14,7 @@ __all__ = [
     "dummy",
     "Gene",
     "Individual",
+    "Population",
     ]
 __author__ = "Stanislav Rubint"
 __year__ = 2020
@@ -43,7 +44,7 @@ class Gene:
 
 
 class Gene:
-    def __init__(self, uname: str, type: geneTypes, value: object, *, dominant: bool = False, rateOfMutation: float = 1, availableOptions: list, max: object = math.inf, min: object = -math.inf):
+    def __init__(self, uname: str, type: geneTypes, value: object, *, dominant: bool = False, rateOfMutation: float = 1, availableOptions: list = [], max: object = math.inf, min: object = -math.inf):
         super().__init__()
         self.uname = uname
         self.type = type
@@ -52,13 +53,28 @@ class Gene:
         self.value = value
         if type == geneTypes.VALUE:
             self.options = availableOptions
+        elif type == geneTypes.NATURAL:
+            self.min = 0
+            self.max = max
         else:
             self.min = min
             self.max = max
 
-    def mutate(self, chance: float = 0):
+    def mutate(self, chance: float) -> Gene:
+        chance = chance*self.rate
+        random = np.random.random()
         if self.type == geneTypes.VALUE:
-            chance = chance*self.rate
+            if chance >= random:
+                self.value = np.random.choice(self.options)
+        else:
+            diff = self.max - self.min
+            delta = diff * ((2 * random) - 1)
+            self.value += delta
+            if self.value < self.min:
+                self.value = self.min
+            elif self.value > self.max:
+                self.value = self.max
+        return self
 
     def __gt__(self, gene: Gene):
         "Return True if dominant over gene"
@@ -81,14 +97,20 @@ class Gene:
 
     def __repr__(self):
         if self.type == geneTypes.VALUE:
-            return f"Gene(uname={self.uname}, type={self.type}, value={self.value}, dominant={self.dominant}, rateOfMutation={self.rate}, availableOptions={self.options})"
+            return f"Gene(uname={self.uname}, type={self.type}, value='{self.value}', dominant={self.dominant}, rateOfMutation={self.rate}, availableOptions={self.options})"
         else:
             return f"Gene(uname={self.uname}, type={self.type}, value={self.value}, dominant={self.dominant}, rateOfMutation={self.rate}, min={self.min}, max={self.max})"
+
+
+class Individual:
+    """Declaration"""
+    pass
 
 
 class Individual(dict):
     def __init__(self, genome={}):
         super().__init__(genome)
+        self["__score__"] = 0
 
     def __iadd__(self, gene):
         self[gene.uname] = gene
@@ -101,14 +123,15 @@ class Individual(dict):
         """Perform uniform crossover with other individual"""
         offspring = Individual()
         for key in self.keys():
-            gA = self.get(key)
-            gB = individual.get(key)
-            if gA == gB:
-                offspring[key] = np.random.choice([gA, gB]).copy()
-            elif gA > gB:
-                offspring[key] = gA.copy()
-            else:
-                offspring[key] = gB.copy()
+            if not key.startswith("__"):
+                gA = self.get(key)
+                gB = individual.get(key)
+                if gA == gB:
+                    offspring[key] = np.random.choice([gA, gB]).copy()
+                elif gA > gB:
+                    offspring[key] = gA.copy()
+                else:
+                    offspring[key] = gB.copy()
 
         return offspring
 
@@ -119,25 +142,46 @@ class Individual(dict):
         sRep = {key: value for (key, value) in self.items()}
         return f"Individual(genome={sRep})"
 
-    def mutate(self):
-        return Individual(self)
+    def mutate(self, chance: float) -> Individual:
+        mutated = self.copy()
+        for key in self.keys():
+            if not key.startswith("__"):
+                self[key].mutate(chance)
+
+        return mutated
 
     def copy(self):
         return Individual(copy.deepcopy(self))
 
 
 class Population(list):
-    def __init__(self, individual: Individual, size: int):
+    def __init__(self, individual: Individual, size: int, chanceOfMutation: float = 0.1):
         self.species = individual
-        super().__init__([individual.mutate() for i in range(size)])
+        self.chanceOfMutation = chanceOfMutation
+        super().__init__([self.species.mutate(chanceOfMutation) for i in range(size)])
 
     def evolve(self, *, timeout: float = 0, iterations: int = 1):
-        if timeout:
-            startTime = time.time()
+        # TODO: Timeout
         for i in range(iterations):
-            pass
-            # TODO:
+            for individual in self:
+                # evaluate each individual according to fitness function
+                individual["__score__"] = self.fitness(individual)
 
-    def fitness(self):
+            # sort population according to fitness score
+            self.sort(key=lambda individual: individual["__score__"])
+            # remove tail of population
+            del self[:int(len(self)/2)]
+            for i in range(0, len(self), 2):
+                # crossover those motherfuckers
+                self.append(self[i]@self[i+1])
+                self.append(self[i]@self[i+1])
+            for individual in self:
+                individual.mutate(self.chanceOfMutation)
+
+    def fitness(self, individual):
         """Override this function with your fitness function"""
-        raise NotImplementedError("You must override fitness method in order to evolve")
+        return len(individual.get("eye_color").value) + individual.get("height").value
+        # raise NotImplementedError("You must override fitness method in order to evolve")
+
+    def __str__(self):
+        return str([(i.get("eye_color").value, i.get("height").value) for i in self])
--- src/test.py
+++ src/test.py
@@ -2,6 +2,9 @@
 
 
 import ga
+import numpy as np
+from timeit import timeit
+import time
 
 
 class Localizations(ga.Population):
@@ -9,13 +12,23 @@ class Localizations(ga.Population):
         pass
 
 
+colors = ["brown", "green", "grey", "blue", "chocolate"]
+
+
 if __name__ == '__main__':
 
     i1, i2 = [ga.Individual() for i in range(2)]
 
-    i1 += ga.Gene("name", ga.geneTypes.VALUE, "Bob", availableOptions=["Mark", "John"], dominant=True)
-    i2 += ga.Gene("name", ga.geneTypes.VALUE, "Jossie", availableOptions=["Rachel", "Lisa"])
+    i1 += ga.Gene("eye_color", ga.geneTypes.VALUE, np.random.choice(colors), availableOptions=colors, dominant=True)
+    i1 += ga.Gene("height", ga.geneTypes.NATURAL, np.random.randint(100, 200), min=1, max=300)
 
-    pop = Localizations(i1, 40)
-    pop.fitness()
-    pop.evolve()
+    pop = ga.Population(i1, 100)
+    print(pop)
+    tStart = time.time()
+    pop.evolve(iterations=1000)
+    print(pop)
+    print(time.time()-tStart)
+
+
+    # print(timeit(stmt='pop = ga.Population(ga.Individual({"name":ga.Gene("name", ga.geneTypes.VALUE, "Bob", availableOptions=["Mark", "John", "BoB"], dominant=True)}), 100)', setup="import ga", number=1000)*1000, "us per population of 100 seed")
+    # print(timeit(stmt='i.mutate(.3)', setup='import ga;i=ga.Individual({"name":ga.Gene("name", ga.geneTypes.VALUE, "Bob", availableOptions=["Mark", "John", "BoB"], dominant=True)})', number=1000)*1000, "us per mutation")
